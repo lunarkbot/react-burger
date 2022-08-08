1f3dea6aa86512cb61160d3539b3fd8a7e042f40
Sergej Bopp <58397321+z-style-org@users.noreply.github.com>
2022-08-09 00:26:51 +0300

    fix: remove inline styles

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './Tabs.module.css';
 
 export default function Tabs() {
   const [current, setCurrent] = useState('one')
 
   return(
-    <div style={{ display: 'flex' }} className="mb-10">
+    <div className={`mb-10 ${styles.tabs}`}>
       <Tab value="one" active={current === 'one'} onClick={setCurrent}>
         Булки
       </Tab>
