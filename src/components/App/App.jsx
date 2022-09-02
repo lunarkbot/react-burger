857cbedffaf343112a1d9100f5abc067fc7bef36
Sergej Bopp <58397321+z-style-org@users.noreply.github.com>
2022-09-02 16:13:58 +0300

    feat: add routing and main page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import styles from './App.module.css';
 import AppHeader from '../AppHeader/AppHeader';
-import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
-import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import { MainPage } from '../../pages';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <AppHeader />
-      <main className={styles.main}>
-        <h1 className="mb-5 text text_type_main-large">Соберите бургер</h1>
-        <div className={styles.twoColumns}>
-          <DndProvider backend={HTML5Backend}>
-            <BurgerIngredients />
-            <BurgerConstructor />
-          </DndProvider>
-        </div>
-      </main>
-    </>
+      <Switch>
+        <Route exact={true} path='/'>
+          <MainPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
